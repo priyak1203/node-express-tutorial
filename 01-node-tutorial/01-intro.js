c32327f718888620ea88fa1f3ad823e1f7c69952
Priydarshini Karanth <kpriya1203@gmail.com>
2022-09-07 13:38:58 +0530

    initial commit intro code

---
+++ 01-node-tutorial/01-intro.js
@@ -0,0 +1,11 @@
+const amount = 12;
+
+console.log('Hello World!');
+
+if (amount < 10) {
+  console.log('small number');
+} else {
+  console.log('large number');
+}
+
+console.log(`Hey its my first node app!!!`);
