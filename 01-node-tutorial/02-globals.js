cafb4e30cde0b5ad538f9ad18655dfea54ccfbb7
Priydarshini Karanth <kpriya1203@gmail.com>
2022-09-07 15:33:04 +0530

    globals

---
+++ 01-node-tutorial/02-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - No Window object available
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to user modules (CommonJS)
+// module     - info about current module(file)
+
+console.log(`The path is ---- ${__dirname}`);
+console.log(`The file name is : ${__filename}`);
+
+setInterval(() => {
+  console.log('Hello World');
+}, 2000);
