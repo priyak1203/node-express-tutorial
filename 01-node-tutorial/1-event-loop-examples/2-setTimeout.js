ec51e5e28aed6cf67d2fa6409f2df01d3c51477a
Priydarshini Karanth <kpriya1203@gmail.com>
2022-09-11 12:51:56 +0530

    setTimeout event loop example

---
+++ 01-node-tutorial/1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,9 @@
+// started operatin system process
+console.log('First');
+setTimeout(() => {
+  console.log('Second');
+}, 0);
+
+console.log('Third');
+
+// completed and exited operatin system process
