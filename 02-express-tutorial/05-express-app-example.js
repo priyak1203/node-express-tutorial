6025922e72ca769d5310a2fcdb4859c9a2894d27
Priydarshini Karanth <kpriya1203@gmail.com>
2022-11-30 14:54:34 +0530

    express app example

---
+++ 02-express-tutorial/05-express-app-example.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// setup static and middleware
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Resource Not Found</h1>');
+});
+
+app.listen(5000, () => console.log('Server listening on PORT: 5000'));
