f3b66f57a0d75f2f59b316ae708cfdf39377f06a
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-04 16:19:38 +0530

    http post javascript form

---
+++ 02-express-tutorial/15-http-post-javascript-form.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const { people } = require('./data');
+
+// static assets
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ succes: true, data: people });
+});
+
+// javascript form
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
+});
+
+// traditional form
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  return res.status(401).send('Please provide credentials');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('Resource Not Found');
+});
+
+app.listen(5000, () => console.log('Server listening on PORT: 5000'));
