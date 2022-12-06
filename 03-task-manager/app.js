49b481b0a5bfd8cd04380b492180f669a3944426
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-06 15:59:47 +0530

    general routes

--- 03-task-manager/app.js
+++ 03-task-manager/app.js
@@ -5,6 +5,13 @@ app.get('/', (req, res) => {
   res.status(200).send('Task Manager App');
 });
 
+// general routes
+// app.get('/api/v1/tasks')             -- get all the tasks
+// app.post('/api/v1/tasks')            -- create a new task
+// app.get('/api/v1/tasks/:id')         -- get a single task
+// app.patch('/api/v1/tasks/:id')       -- update a task
+// app.delete('/api/v1/tasks/:id')      -- delete a task
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server is listening on PORT: ${port}`));
