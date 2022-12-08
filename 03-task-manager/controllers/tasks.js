3ca86e85802165e84d58f36429cb71829e37d553
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-08 12:25:46 +0530

    update task api

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -1,3 +1,4 @@
+const { findOneAndUpdate } = require('../models/Task');
 const Task = require('../models/Task');
 
 const getAllTasks = async (req, res) => {
@@ -33,8 +34,22 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.status(200).json(req.body);
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const deleteTask = async (req, res) => {
