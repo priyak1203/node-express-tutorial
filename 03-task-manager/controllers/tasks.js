05362086b79a3e42d54c74ae76c1412d5bee93fa
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-08 11:23:13 +0530

    get single task api basic

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -18,9 +18,19 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  const { id } = req.params;
-  res.status(200).json({ id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
