b2f4620c89efa320f94ebda0345bf39d326354f6
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-10 12:15:12 +0530

    response type options

--- 03-task-manager/controllers/tasks.js
+++ 03-task-manager/controllers/tasks.js
@@ -4,6 +4,15 @@ const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({ tasks });
+
+    // alternate options
+    // res.status(200).json({ tasks, amount: tasks.length });
+    // res
+    //   .status(200)
+    //   .json({ success: true, data: { tasks, nbHits: tasks.length } });
+    // res
+    //   .status(200)
+    //   .json({ status: 'success', data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -70,31 +79,10 @@ const deleteTask = async (req, res) => {
   }
 };
 
-// Put method to over write the document
-const editTask = async (req, res) => {
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      new: true,
-      runValidators: true,
-      overwrite: true,
-    });
-
-    if (!task) {
-      return res.status(404).json({ msg: `no task with id: ${taskID}` });
-    }
-
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
-};
-
 module.exports = {
   getAllTasks,
   createTask,
   getTask,
   updateTask,
   deleteTask,
-  editTask,
 };
