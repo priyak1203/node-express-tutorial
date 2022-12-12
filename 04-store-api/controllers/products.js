8b7e7cdad4fd74c7fb2ef1be585bb35e4997c17b
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-12 12:16:43 +0530

    accessing query params

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -5,8 +5,9 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ nbHits: products.length, products });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).send('Get all products');
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
