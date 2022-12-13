6b72755c00de8f88888448ceb52e31a089a0f927
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-13 11:37:41 +0530

    sort functionality implemented

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require('../models/Product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({});
+  const products = await Product.find({}).sort('-name');
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
+
   const queryObject = {};
 
   if (featured) {
@@ -21,8 +22,17 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
 
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
