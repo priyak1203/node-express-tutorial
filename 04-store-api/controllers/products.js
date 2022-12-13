bf557f892f94eaada0bed4642ba6a558b16eb9e2
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-13 15:28:41 +0530

    pagination using skip and limit

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/Product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('-name -price');
+  const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .limit(10)
+    .skip(1);
   res.status(200).json({ nbHits: products.length, products });
 };
 
@@ -37,6 +41,13 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ nbHits: products.length, products });
