5c27fb8913ba623892ac6303a4727342a1a47f41
Priydarshini Karanth <kpriya1203@gmail.com>
2022-12-14 16:29:00 +0530

    basic express app

---
+++ 05-JWT-basics/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const start = () => {
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
