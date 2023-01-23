e0e2722a11add9ccd9a256fb117bc19496cdfaaf
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-23 15:27:52 +0530

    login user basic setup in controller

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnAuthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -9,7 +10,20 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login User');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  // compare password
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
