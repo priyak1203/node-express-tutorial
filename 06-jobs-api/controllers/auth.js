e40daf8997d8abe1c222c202e6206aa2ce1aba85
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-23 11:32:29 +0530

    mongoose instance method example

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -8,7 +8,9 @@ const register = async (req, res) => {
     expiresIn: '30d',
   });
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.getName() }, token });
 };
 
 const login = async (req, res) => {
