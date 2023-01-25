3e3c2d843367467ae154f72cb6106c7317d17572
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-25 11:32:09 +0530

    duplicate email error handling

--- 06-jobs-api/middlewares/error-handler.js
+++ 06-jobs-api/middlewares/error-handler.js
@@ -2,10 +2,26 @@ const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandler = (err, req, res, next) => {
+  let customError = {
+    // set defaults
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || `Something went wrong, try again later`,
+  };
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err });
+
+  // duplicate email error
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandler;
