936301a12b495c6b334fc8bf98ba70e31f36c2d2
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-25 13:02:53 +0530

    installing and setting up security packages

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -3,6 +3,12 @@ const app = express();
 require('dotenv').config();
 require('express-async-errors');
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // database connection
 const connectDB = require('./db/connect');
 
@@ -18,7 +24,19 @@ const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 // middlewares
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window`
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // set up routes
 app.use('/api/v1/auth', authRouter);
