43b4a39ad738af85cd4a861c5871e242b35aaedd
Priydarshini Karanth <kpriya1203@gmail.com>
2023-01-25 11:03:03 +0530

    remove job controller

--- 06-jobs-api/controllers/jobs.js
+++ 06-jobs-api/controllers/jobs.js
@@ -53,7 +53,18 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete Job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ msg: 'Deleted Successfully', job });
 };
 
 module.exports = {
