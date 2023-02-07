3d27c425f8838837b1184a567426653218fbed5b
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-07 16:24:55 +0530

    small sidebar

--- 06-jobster-api/client/src/components/SmallSidebar.js
+++ 06-jobster-api/client/src/components/SmallSidebar.js
@@ -1,7 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import Logo from './Logo';
+import { toggleSidebar } from '../features/user/userSlice';
+import Navlinks from './Navlinks';
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <Navlinks toggleSidebar={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
