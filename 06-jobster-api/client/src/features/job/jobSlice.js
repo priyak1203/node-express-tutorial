e90aa4c809f6264670468d0d6257dc492b58a892
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-09 13:09:52 +0530

    job slice basic structure

---
+++ 06-jobster-api/client/src/features/job/jobSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoading: false,
+  position: '',
+  company: '',
+  jobLocation: '',
+  jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+  jobType: 'full-time',
+  statusOptions: ['interview', 'declined', 'pending'],
+  status: 'pending',
+};
+
+const jobSlice = createSlice({
+  name: 'job',
+  initialState,
+});
+
+export default jobSlice.reducer;
