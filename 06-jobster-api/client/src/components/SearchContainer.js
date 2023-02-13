a4efbbe2cf7ec73d5bd7d864bb07283b68ba6ae2
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-13 15:15:43 +0530

    search container ui implemented

--- 06-jobster-api/client/src/components/SearchContainer.js
+++ 06-jobster-api/client/src/components/SearchContainer.js
@@ -1,5 +1,66 @@
+import Wrapper from '../assets/wrappers/SearchContainer';
+import { FormRow, FormRowSelect } from '.';
+import { useSelector } from 'react-redux';
+
 const SearchContainer = () => {
-  return <h2>SearchContainer</h2>;
+  const { isLoading, search, searchType, searchStatus, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { statusOptions, jobTypeOptions } = useSelector((store) => store.job);
+
+  const handleSearch = (e) => {
+    console.log(e.target.name, e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h4>search form</h4>
+        <div className="form-center">
+          {/* search position */}
+          <FormRow
+            name="search"
+            type="text"
+            value={search}
+            handleChange={handleSearch}
+          />
+          {/* search by job status */}
+          <FormRowSelect
+            name="searchStatus"
+            value={searchStatus}
+            list={['all', ...statusOptions]}
+            handleChange={handleSearch}
+            labelText="status"
+          />
+          {/* search by job type */}
+          <FormRowSelect
+            name="searchType"
+            value={searchType}
+            list={['all', ...jobTypeOptions]}
+            handleChange={handleSearch}
+            labelText="type"
+          />
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            list={sortOptions}
+            handleChange={handleSearch}
+          />
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="btn btn-block btn-danger"
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
--- 06-jobster-api/client/src/features/allJobs/allJobSlice.js
+++ 06-jobster-api/client/src/features/allJobs/allJobSlice.js
@@ -3,6 +3,14 @@ import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
 import { logoutUser } from '../user/userSlice';
 
+const initialFiltersState = {
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+};
+
 const initialState = {
   isLoading: false,
   jobs: [],
@@ -10,6 +18,7 @@ const initialState = {
   numOfPages: 1,
   stats: {},
   monthlyApplications: [],
+  ...initialFiltersState,
 };
 
 export const getAllJobs = createAsyncThunk(
