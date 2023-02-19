a20499db7a800f235af595a500c5ad98fdc5442e
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-19 16:20:50 +0530

    error handlers implementation

--- 06-jobster-api/middleware/error-handler.js
+++ 06-jobster-api/middleware/error-handler.js
@@ -1,11 +1,35 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    // set defaults
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || `Something went wrong, try again later`,
+  };
+
+  // validation error
+  if (err.name === 'ValidationError') {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err });
+
+  // duplicate email error
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+  }
+
+  // cast error
+  if (err.name === 'CastError') {
+    customError.statusCode = StatusCodes.NOT_FOUND;
+    customError.msg = `No item found with id: ${err.value}`;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandler;
