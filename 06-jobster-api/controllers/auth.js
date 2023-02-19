e9d1894f4e4ea6dbcdb2b3dced77b0e86e7bd603
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-19 12:20:57 +0530

    login user functionality

--- 06-jobster-api/controllers/auth.js
+++ 06-jobster-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnAuthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -9,7 +10,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login User');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  // compare password
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
