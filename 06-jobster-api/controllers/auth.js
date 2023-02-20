f9f4b8c260e0a3b139330b92985da646b7643395
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-20 13:16:16 +0530

    updateUser route and controller

--- 06-jobster-api/controllers/auth.js
+++ 06-jobster-api/controllers/auth.js
@@ -47,7 +47,41 @@ const login = async (req, res) => {
   });
 };
 
+const updateUser = async (req, res) => {
+  const { name, lastName, email, location } = req.body;
+  if (!name || !lastName || !email || !location) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+  user.name = name;
+  user.lastName = lastName;
+  user.email = email;
+  user.location = location;
+
+  await user.save();
+  const token = user.createJWT();
+
+  // Alternative approach which doesnt hash the password multiple times
+  // const user = await User.findOneAndUpdate({ _id: req.user.userId }, req.body, {
+  //   new: true,
+  //   runValidators: true,
+  // });
+  // const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      name: user.name,
+      lastName: user.lastName,
+      location: user.location,
+      token,
+    },
+  });
+};
+
 module.exports = {
   register,
   login,
+  updateUser,
 };
--- 06-jobster-api/routes/auth.js
+++ 06-jobster-api/routes/auth.js
@@ -1,8 +1,10 @@
-const { register, login } = require('../controllers/auth');
+const { register, login, updateUser } = require('../controllers/auth');
+const authenticateUser = require('../middleware/authentication');
 const express = require('express');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.patch('/updateUser', authenticateUser, updateUser);
 
 module.exports = router;
