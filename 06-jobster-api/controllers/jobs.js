12d1ba9d59b074e6ae1d2b0184b949b72275d3ce
Priydarshini Karanth <kpriya1203@gmail.com>
2023-02-22 10:34:43 +0530

    pagination implementation

--- 06-jobster-api/controllers/jobs.js
+++ 06-jobster-api/controllers/jobs.js
@@ -5,7 +5,7 @@ const Job = require('../models/Job');
 const getAllJobs = async (req, res) => {
   const { status, jobType, sort, search } = req.query;
 
-  // Search the jobs of specific user
+  // Search the jobs of a specific user
   const queryObject = {
     createdBy: req.user.userId,
   };
@@ -42,8 +42,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
+  // pagination
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
