f935f411072a53d984dc28d3eb380a8f773d4711
Priydarshini Karanth <kpriya1203@gmail.com>
2023-03-15 15:29:30 +0530

    get all products controller function

--- 07-file-upload/controllers/productController.js
+++ 07-file-upload/controllers/productController.js
@@ -6,8 +6,9 @@ const createProduct = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ product });
 };
 
-const getAllProducts = (req, res) => {
-  res.send('Get All Products');
+const getAllProducts = async (req, res) => {
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
