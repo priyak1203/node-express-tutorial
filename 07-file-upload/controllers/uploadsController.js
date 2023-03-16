537e17f146756897bdcfbb63f726903a957f6051
Priydarshini Karanth <kpriya1203@gmail.com>
2023-03-16 13:18:03 +0530

    additional checks for file type and size

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,8 +1,28 @@
 const { StatusCodes } = require('http-status-codes');
 const path = require('path');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
+  // check if the file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded!');
+  }
   const productImage = req.files.image;
+
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please upload Image');
+  }
+
+  // check size
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please upload image smaller than 1MB'
+    );
+  }
+
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
