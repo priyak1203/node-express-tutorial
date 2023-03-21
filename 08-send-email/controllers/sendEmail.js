2257568b4d34a68325845e63f1dee913a7b42ca3
Priydarshini Karanth <kpriya1203@gmail.com>
2023-03-21 13:51:43 +0530

    sending emails using ethereal

--- 08-send-email/controllers/sendEmail.js
+++ 08-send-email/controllers/sendEmail.js
@@ -1,5 +1,24 @@
-const sendEmail = (req, res) => {
-  res.send('Send email func');
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (req, res) => {
+  const testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'jamar.huels7@ethereal.email',
+      pass: 'cQKgjxPbRtumdvhrWr',
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Pegasus Consulting" <pegasusinternational@gmail.com>',
+    to: 'bar@example.com',
+    subject: 'Hello There!',
+    html: '<h2>Sending Emails with Node.js</h2>',
+  });
+  res.json(info);
 };
 
 module.exports = sendEmail;
