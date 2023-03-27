174809faf40346e141942eaa436976bd0537b685
Priydarshini Karanth <kpriya1203@gmail.com>
2023-03-27 16:16:32 +0530

    stripe payment basic setup

---
+++ 09-stripe-payment/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+require('express-async-errors');
+
+// error handler
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+
+app.get('/', (req, res) => {
+  res.send('Stripe Payment Project');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = () => {
+  try {
+    app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
