ac108a04a21fda620c5aa64025364b6bf6c39f5b
Priydarshini Karanth <kpriya1203@gmail.com>
2023-04-17 15:09:52 +0530

    database connection setup

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+// database connection
+const connectDB = require('./db/connect');
+
 app.get('/', (req, res) => {
   res.send('Home Page');
 });
 
 const PORT = process.env.PORT || 3000;
-
-const start = () => {
+const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
   } catch (error) {
     console.log(error);
