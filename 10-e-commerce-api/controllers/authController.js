c1ba569176097027f89a345aab4f298d4c01ad0c
Priydarshini Karanth <kpriya1203@gmail.com>
2023-04-20 15:41:21 +0530

    register controller basic functionality

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -1,5 +1,18 @@
-const register = (req, res) => {
-  res.send('Register User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+  const { email } = req.body;
+
+  // checking for duplicate email value
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = (req, res) => {
