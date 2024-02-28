2c11676266e9872f9c81dbabaf283fdb648d6f03
Priydarshini Karanth <kpriya1203@gmail.com>
2024-02-28 15:18:09 +0530

    create token user refactoring

--- 10-e-commerce-api/controllers/authController.js
+++ 10-e-commerce-api/controllers/authController.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const User = require('../models/User');
-const { attachCookiesToResponse } = require('../utils');
+const { attachCookiesToResponse, createTokenUser } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -18,7 +18,7 @@ const register = async (req, res) => {
   const user = await User.create({ email, name, password, role });
 
   // jwt config
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, tokenUser });
 
   res.status(StatusCodes.CREATED).json({ user: tokenUser });
@@ -45,7 +45,7 @@ const login = async (req, res) => {
   }
 
   // attach cookies
-  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, tokenUser });
 
   res.status(StatusCodes.OK).json({ user: tokenUser });
