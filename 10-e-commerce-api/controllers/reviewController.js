a5f14ae3ffbe095fcca42195380466cb06dcd980
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-01 11:53:42 +0530

    create review controller implementation

--- 10-e-commerce-api/controllers/reviewController.js
+++ 10-e-commerce-api/controllers/reviewController.js
@@ -1,5 +1,34 @@
+const { StatusCodes } = require('http-status-codes');
+const Review = require('../models/Review');
+const Product = require('../models/Product');
+const CustomError = require('../errors');
+
 const createReview = async (req, res) => {
-  res.send('Create review');
+  const { userId } = req.user;
+  const { product: productId } = req.body;
+
+  // check if the product exists
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  // check if the user has already reviewed the product
+  const alreadyReviewed = await Review.findOne({
+    user: userId,
+    product: productId,
+  });
+
+  if (alreadyReviewed) {
+    throw new CustomError.BadRequestError(
+      'Already submitted review for this product'
+    );
+  }
+
+  req.body.user = userId;
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
