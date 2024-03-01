2f99f84efd6adaf3fa740c6dfc4caf2f5907d3a8
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-01 20:12:47 +0530

    update review controller implementation

--- 10-e-commerce-api/controllers/reviewController.js
+++ 10-e-commerce-api/controllers/reviewController.js
@@ -49,7 +49,26 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-  res.send('Update Review');
+  const { id: reviewId } = req.params;
+  const { rating, title, comment } = req.body;
+
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
+  }
+
+  // check permissions
+  checkPermissions({
+    requestUser: req.user,
+    resourceUserId: review.user,
+  });
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+  await review.save();
+
+  res.status(StatusCodes.OK).json({ review });
 };
 
 const deleteReview = async (req, res) => {
@@ -60,8 +79,11 @@ const deleteReview = async (req, res) => {
     throw new CustomError.NotFoundError(`No review with id: ${reviewId}`);
   }
 
-  //   Check permissions
-  checkPermissions({ requestUser: req.user, resourceUserId: review.user });
+  // check permission to delete
+  checkPermissions({
+    requestUser: req.user,
+    resourceUserId: review.user,
+  });
 
   await review.deleteOne();
   res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' });
