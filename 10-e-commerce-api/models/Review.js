0caae3025c52a6479ed91f6e6a7ddb2b42d5baa0
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-02 23:47:34 +0530

    average ratings and no of reviews calculation

--- 10-e-commerce-api/models/Review.js
+++ 10-e-commerce-api/models/Review.js
@@ -35,7 +35,32 @@ const ReviewSchema = new mongoose.Schema(
 ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 ReviewSchema.statics.calculateAvgRating = async function (productId) {
-  console.log(productId);
+  const result = await this.aggregate([
+    {
+      $match: {
+        product: productId,
+      },
+    },
+    {
+      $group: {
+        _id: '$product',
+        averageRating: { $avg: '$rating' },
+        numOfReviews: { $sum: 1 },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Product').findOneAndUpdate(
+      { _id: productId },
+      {
+        averageRating: Math.ceil(result[0]?.averageRating) || 0,
+        numOfReviews: result[0]?.numOfReviews || 0,
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 ReviewSchema.post('save', async function () {
