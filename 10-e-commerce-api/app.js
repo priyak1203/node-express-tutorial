12b83d40e439be953bc6aacc56dd1eb761e886ba
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-06 16:20:56 +0530

    security packages installation and setup

--- 10-e-commerce-api/app.js
+++ 10-e-commerce-api/app.js
@@ -7,6 +7,11 @@ require('express-async-errors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // database connection
 const connectDB = require('./db/connect');
@@ -23,6 +28,18 @@ const notFoundMiddleware = require('./middlewares/not-found');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 // setup middlewares
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
