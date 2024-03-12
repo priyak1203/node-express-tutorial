ef40b8834f1a53aa0187e8e329c64e347ce29c74
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-13 00:45:48 +0530

    login page ui added

--- 11-auth-workflow/client/src/pages/Login.jsx
+++ 11-auth-workflow/client/src/pages/Login.jsx
@@ -1,4 +1,53 @@
+import styled from 'styled-components';
+import FormRow from '../components/FormRow';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleChange = (e) => {};
+
+  return (
+    <Wrapper className="page">
+      <form className="form">
+        <FormRow type="email" name="email" handleChange={handleChange} />
+        <FormRow type="password" name="password" handleChange={handleChange} />
+        <button type="submit" className="btn btn-block">
+          login
+        </button>
+        <p>
+          Don't have an account?
+          <Link to="/register" className="register-link">
+            register
+          </Link>
+        </p>
+        <p>
+          Forgot your password?
+          <Link to="/" className="reset-link">
+            reset password
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 export default Login;
+
+const Wrapper = styled.section`
+  p {
+    margin: 0;
+    text-align: center;
+  }
+  .btn {
+    margin-bottom: 1.5rem;
+  }
+  .register-link,
+  .reset-link {
+    display: inline-block;
+    margin-left: 0.25rem;
+    text-transform: capitalize;
+    color: var(--primary-500);
+    cursor: pointer;
+  }
+  .reset-link {
+    margin-top: 0.25rem;
+  }
+`;
