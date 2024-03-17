cdd3995d11192903d9640e1de65c4ffcd9f2d62d
Priydarshini Karanth <kpriya1203@gmail.com>
2024-03-17 15:34:31 +0530

    change the verification link to 5000

--- 11-auth-workflow/controllers/authController.js
+++ 11-auth-workflow/controllers/authController.js
@@ -31,7 +31,7 @@ const register = async (req, res) => {
     verificationToken,
   });
 
-  const origin = 'http://localhost:5173';
+  const origin = 'http://localhost:5000';
 
   await sendVerificationEmail({
     name: user.name,
